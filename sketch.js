becfc82801bdb193e527a15f8b5c529a64666f24
Joshua Peck <peckjj@dukes.jmu.edu>
2020-08-16 15:53:34 -0400

    initialized colors in setup

--- sketch.js
+++ sketch.js
@@ -23,8 +23,8 @@ var CPU_DEPTH = 4;
 var nodes_explored = 0;
 var boardImage;
 
-var CPU_COLOR = color(255, 204, 0);
-var PLAYER_COLOR = color(255, 100, 100);
+var CPU_COLOR;
+var PLAYER_COLOR;
 
 function preload()
 {
@@ -33,6 +33,9 @@ function preload()
 
 function setup() 
 {
+  CPU_COLOR = color(255, 204, 0);
+  PLAYER_COLOR = color(255, 100, 100);
+
   createCanvas(windowWidth / 2, windowHeight / 2, P2D);
   background(0);
   image(boardImage, 0, 0, width, height);
